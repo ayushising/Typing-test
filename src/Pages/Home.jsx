635cff0285ad3fbfb674a2f9c92ae925d302dee7
Ayushi_Singh <66073819+ayushising@users.noreply.github.com>
2022-12-04 12:58:55 +0530

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import Footer from '../Components/Footer'
+import Header from '../Components/Header'
+import TypingBox from '../Components/TypingBox'
+import { useTheme } from '../Context/ThemeContext'
+import { GlobalStyles } from '../Styles/global'
+
+const HomePage = () => {
+
+    // const {theme} = useTheme();
+  
+    return (
+      <div className="canvas">
+        <Header/>
+        <TypingBox/>
+        <Footer/>
+      </div>
+  )
+}
+
+export default HomePage
