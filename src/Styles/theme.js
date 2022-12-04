ba4302802b39f3a28e788711ec3b254e1228c0bd
Ayushi_Singh <66073819+ayushising@users.noreply.github.com>
2022-12-04 12:58:12 +0530

    Add files via upload

---
+++ src/Styles/theme.js
@@ -0,0 +1,53 @@
+const darkTheme = {
+  label: "Dark",
+  background: "black",
+  title: "red",
+  typeBoxText: "grey",
+  stats: "green",
+};
+
+const redTheme = {
+  label: "Red Theme",
+  background: "red",
+  title: "white",
+  typeBoxText: "blue",
+  stats: "purple",
+};
+
+const Blue = {
+  label: "Blue",
+  background: "#000066",
+  title: "#F67599",
+  typeBoxText: "#9999ff",
+  stats: "#F67599",
+};
+const Green = {
+  label: "Green",
+  background: "#008000",
+  title: "#F67599",
+  typeBoxText: "#9999ff",
+  stats: "#F67599",
+};
+const Brown = {
+  label: "Brown",
+  background: "#663300",
+  title: "red",
+  typeBoxText: "grey",
+  stats: "green",
+};
+const Yellow = {
+  label: "Yellow",
+  background: "#e6e600",
+  title: "red",
+  typeBoxText: "grey",
+  stats: "green",
+};
+
+export const themeOptions = [
+  { value: darkTheme, label: "Dark" },
+  { value: redTheme, label: "Red Theme" },
+  { value: Blue, label: "Blue" },
+  { value: Green, label: "Green" },
+  { value: Yellow, label: "Yellow" },
+  { value: Brown, label: "Brown" },
+];
